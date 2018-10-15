437ba1913fa16608d3f8d525fc6d92f6d4b582f6
alejandro <43358869+alejandroTESEE@users.noreply.github.com>
2018-10-14 20:47:31 -0500

    Add files via upload

---
+++ tarea 5 billetes y moneda.cpp
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+
+int main (void)
+{
+    int a;
+    int p=1;
+    int q=2;
+    int r=5;
+    int s=10;
+    int t=20;
+    int v=50;
+    int w=100;
+    int x=200;
+    int y=500;
+    int z=1000;
+    
+    
+    printf("introduce un valor: ",a);
+    scanf("%d",a);
+    if(a % a== p+q+r+s+t+v+w+x+y+z)
+    printf("\n a es igual a: %d",&p,&q,&r,&s,&t,&v,&w,&x,&y,&z);
+    
+    getch();
+    return 0;
+
+}
